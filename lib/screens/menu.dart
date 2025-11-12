1f22cc3447f9169bb1ab3a4204a2f4ddfdb78201
Nazwa Zahra Sausan <nsausan72@gmail.com>
2025-11-12 15:23:43 +0700

    Tutorial 8: Completed

--- lib/screens/menu.dart
+++ lib/screens/menu.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:football_news/widgets/left_drawer.dart';
 import 'package:football_news/widgets/news_card.dart';
+import 'package:football_news/screens/news_entry_list.dart';
 
 class MyHomePage extends StatelessWidget {
     MyHomePage({super.key});
