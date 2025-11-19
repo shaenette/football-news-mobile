a3f8ddb6e4f6a452ce12b7876fe7529e54e6755e
Nazwa Zahra Sausan <nsausan72@gmail.com>
2025-11-19 14:13:45 +0700

    trigger

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:football_news/screens/menu.dart';
 import 'package:pbp_django_auth/pbp_django_auth.dart';
 import 'package:provider/provider.dart';
 import 'package:football_news/screens/login.dart';
